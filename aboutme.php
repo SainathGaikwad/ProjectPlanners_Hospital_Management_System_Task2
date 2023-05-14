3bb98ffc9538a15a9e6ce71f7b050a1d7301bb66
Sainath Atmaram Gaikwad <89898283+SainathGaikwad@users.noreply.github.com>
2023-05-14 12:52:44 +0530

    update

--- aboutme.php
+++ aboutme.php
@@ -1,17 +1,16 @@
 <?php
-//includes page that detects if the database is connected
 include 'session.php';
-$user = $_SESSION['user'];//assigns the current session as the user of the filesystem
+$user = $_SESSION['user'];
  ?>
-<!--the first page redirected from login-->
- <html>
- <title>Doctor</title>
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 
- <body>
-   <h1 class="title">Welcome <?php echo $user;?>!</h1>
-   <div align="center">
+<html>
+<title>Doctor</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+
+<body>
+    <h1 class="title">Welcome <?php echo $user;?>!</h1>
+    <div align="center">
         <?php
         $patients = "SELECT P.patient_ID, P.Fname, P.Lname, P.Minit, P.Weight, P.Height, P.blood_type, P.chief_complaint, P.diagnosis FROM patient AS P, employee_has_patient AS EP
         where EP.patient_patient_ID = P.patient_ID AND EP.employee_Type = 1
@@ -29,28 +28,32 @@ $user = $_SESSION['user'];//assigns the current session as the user of the files
           }
           echo "</table";
          ?>
-       </div>
+    </div>
 
-   <a href="logout.php">Logout</a>
- </body>
- <style>
-
-  body{
+    <a href="logout.php">Logout</a>
+</body>
+<style>
+body {
     text-align: center;
     color: white;
-   background: #B24592;  /* fallback for old browsers */
-   background: -webkit-linear-gradient(to right, #F15F79, #B24592);  /* Chrome 10-25, Safari 5.1-6 */
-   background: linear-gradient(to right, #F15F79, #B24592); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: #B24592;
+    background: -webkit-linear-gradient(to right, #F15F79, #B24592);
+    background: linear-gradient(to right, #F15F79, #B24592);
 
-  }
-  table, tr, td{
+}
+
+table,
+tr,
+td {
     text-align: center;
     border: 1px solid black;
     font-size: 20px;
 
-  }
-  .title{
+}
+
+.title {
     color: white;
-  }
- </style>
- </html>
+}
+</style>
+
+</html>
